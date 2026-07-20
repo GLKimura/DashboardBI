63cce7ebd12bd2a527093e653483e67516a321ab
GLKimura <contatogabrielkimura@outlook.com>
2026-07-20 16:01:53 -0300

    DashBoardBI

--- resources/views/bi.blade.php
+++ resources/views/bi.blade.php
@@ -10,10 +10,24 @@
         <div class="bg-white shadow-sm rounded-xl border border-gray-200 p-8">
 
             <p class="text-gray-600 mb-6">
-                Clique abaixo para acessar o relatório oficial no Power BI.
+                Clique abaixo para acessar o relatório de Vendas no Power BI.
             </p>
 
-            <a href="https://app.powerbi.com/reportEmbed?reportId=f5759058-e30f-4a9c-b45d-e27e9c33b90c&autoAuth=true&ctid=bf0c1a5b-c591-47cf-97dc-965d49753d69"
+            <a href="https://app.powerbi.com/reportEmbed?reportId=cb3827e6-1eef-4196-8723-912134af4617&autoAuth=true&ctid=bf0c1a5b-c591-47cf-97dc-965d49753d69"
+                target="_blank"
+                class="inline-flex items-center gap-2 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md transition">
+                    🔗 Acessar Dashboard
+            </a>
+
+        </div>
+
+        <div class="bg-white shadow-sm rounded-xl border border-gray-200 p-8">
+
+            <p class="text-gray-600 mb-6">
+                Clique abaixo para acessar o relatório de Antecipação no Power BI.
+            </p>
+
+            <a href="https://app.powerbi.com/reportEmbed?reportId=4042db29-b73c-44b7-8fda-e22a9ae32927&autoAuth=true&ctid=bf0c1a5b-c591-47cf-97dc-965d49753d69"
                 target="_blank"
                 class="inline-flex items-center gap-2 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md transition">
                     🔗 Acessar Dashboard
